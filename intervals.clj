19cd0325eb581b6e3b4e778bcbd27f447dc9196d
Søren Sjørup <zorren@gmail.com>
2020-10-02 10:48:05 +0200

    fix normalization

--- intervals.clj
+++ intervals.clj
@@ -41,10 +41,19 @@
      [[lower upper]]
      (drop-while (fn [[_ b2]] (<= b2 upper)) greater))))
 
-(defn normalize [set]
-  (reduce (fn [acc [a b]] (if (and (last acc) (= (inc (second (last acc))) a))
-                            (conj (butlast acc) [(first (last acc)) b])
-                            (conj acc [a b]))) [] set))
+(defn normalize [[i1 i2 & set]]
+  (cond
+    (nil? i1)
+    ()
+
+    (nil? i2)
+    (list i1)
+
+    (= (last i1) (dec (first i2)))
+    (normalize (cons [(first i1) (last i2)] set))
+
+    :else
+    (cons i1 (normalize (conj set i2)))))
 
 (defn union [s1 s2] (normalize (reduce insert s1 s2)))
 
@@ -53,63 +62,103 @@
 (def long? (partial instance? Long))
 
 (def all-values [Long/MIN_VALUE Long/MAX_VALUE])
+
 (defn all-values? [[a b]] (and (= a Long/MIN_VALUE) (= b Long/MAX_VALUE)))
 
 (defn intervals->set [intervals]
   (reduce
    (fn [acc [v interval-set]]
      (cond
-       (all-values? (first interval-set))
-       acc
-
        (empty? interval-set)
        (reduced :empty-env)
+
+       (all-values? (first interval-set))
+       acc
 
        :else
        (assoc acc v interval-set)))
    {}
    intervals))
 
-(defn exp->set [[op p1 p2]]
-  (when (and ('#{= <= >= < >} op) (or (not (symbol? p1)) (not (long? p2))))
-    (throw (ex-info "only var to long supported" {:op op :p1 p1 :p2 p2})))
-  (case op
-    = {p1 [[p2 p2]]}
-    <= (if-not (= p2 Long/MAX_VALUE) {p1 [[Long/MIN_VALUE p2]]} {})
-    >= (if-not (= p2 Long/MIN_VALUE) {p1 [[p2 Long/MAX_VALUE]]} {})
-    < (if-not (= p2 Long/MIN_VALUE) {p1 [[Long/MIN_VALUE (dec p2)]]} :empty-env)
-    > (if-not (= p2 Long/MAX_VALUE) {p1 [[(inc p2) Long/MAX_VALUE]]} :empty-env)
-    not
-    (let [env (exp->set p1)]
-      (if (= :empty-env env)
-        {}
-        (intervals->set (map (juxt key (comp complement val)) env))))
-    and
-    (let [[env1 env2] [(exp->set p1) (exp->set p2)]]
-      (cond
-        (= :empty-env env1)
-        :empty-env
+(defn env-complement [env]
+  (cond
+    (= env :empty-env)
+    {}
 
-        (= :empty-env env2)
-        :empty-env
+    (= env {})
+    :empty-env
 
-        :else
-        (intervals->set (map (fn [v] [v (intersection (env1 v [all-values]) (env2 v [all-values]))])
-                             (distinct (concat (keys env1) (keys env2)))))))
-    or
-    (let [[env1 env2] [(exp->set p1) (exp->set p2)]]
-      (cond
-        (= :empty-env env1)
-        env2
+    :else
+    (intervals->set (map (juxt key (comp complement val)) env))))
 
-        (= :empty-env env2)
-        env1
+(defn env-intersection [env1 env2]
+  (cond
+    (= env1 :empty-env)
+    :empty-env
 
-        :else
-        (intervals->set (map (fn [v] [v (union (env1 v [all-values]) (env2 v [all-values]))])
-                             (distinct (concat (keys env1) (keys env2)))))))
+    (= env2 :empty-env)
+    :empty-env
 
-    (throw (ex-info "exp->set: no match" {:op op :p1 p1 :p2 p2}))))
+    :else
+    (intervals->set (map (fn [v] [v (intersection (env1 v [all-values]) (env2 v [all-values]))])
+                         (distinct (concat (keys env1) (keys env2)))))))
+
+(defn env-union [env1 env2]
+  (cond
+    (= env1 :empty-env)
+    env2
+
+    (= env2 :empty-env)
+    env1
+
+    :else
+    (intervals->set (map (fn [v] [v (union (env1 v [all-values]) (env2 v [all-values]))])
+                         (distinct (concat (keys env1) (keys env2)))))))
+
+(defn exp->set [e]
+  (cond
+    (= true e)
+    {}
+
+    (= false e)
+    :empty-env
+
+    (list? e)
+    (let [[op p1 p2 p3] e]
+      (when (and ('#{= <= >= < >} op) (or (not (symbol? p1)) (not (long? p2))))
+        (throw (ex-info "only var to long supported" {:op op :p1 p1 :p2 p2})))
+      (case op
+        = {p1 [[p2 p2]]}
+        <= (if-not (= p2 Long/MAX_VALUE) {p1 [[Long/MIN_VALUE p2]]} {})
+        >= (if-not (= p2 Long/MIN_VALUE) {p1 [[p2 Long/MAX_VALUE]]} {})
+        < (if-not (= p2 Long/MIN_VALUE) {p1 [[Long/MIN_VALUE (dec p2)]]} :empty-env)
+        > (if-not (= p2 Long/MAX_VALUE) {p1 [[(inc p2) Long/MAX_VALUE]]} :empty-env)
+        not
+        (env-complement (exp->set p1))
+        and
+        (env-intersection (exp->set p1) (exp->set p2))
+        or
+        (env-union (exp->set p1) (exp->set p2))
+
+        if
+        (let [env-cond (exp->set p1)
+              env-t (exp->set p2)
+              env-f (exp->set p3)]
+          (cond
+            (= env-cond :empty-env)
+            env-f
+
+            (= env-cond {})
+            env-t
+
+            :else
+            (env-union
+             (env-intersection env-cond env-t)
+             (env-intersection (env-complement env-cond) env-f))
+            )
+          )
+
+        (throw (ex-info "exp->set: no match" {:op op :p1 p1 :p2 p2}))))))
 
 (def swapped-op
   '{= =
@@ -122,6 +171,21 @@
   (if (and (long? p1) (symbol? p2))
     (conj (list p2 p1) (swapped-op op))
     (list op p1 p2)))
+
+(def negated-op
+  '{= not=
+    not= =
+    <= >
+    >= <
+    < >=
+    > <=})
+
+(defn negate-comparison [[op & params]]
+  (conj
+   params
+   (or (negated-op op)
+       (throw (ex-info "negate-comparison: operator not supported"
+                       {:op op :params params})))))
 
 (comment
   (complement [[5 10] [11 20]])
@@ -138,6 +202,13 @@
   (normalize [[1 2] [3 3] [4 4]])
   (normalize [])
   (normalize [[Long/MIN_VALUE Long/MAX_VALUE]])
+  (normalize [])
+  (normalize [[1 3]])
+  (normalize [[1 3] [5 7] [8 9]])
+  (normalize [[1 2] [4 5] [7 9]])
+  (normalize [all-values])
+  (-> (insert [[3 4] [11 9223372036854775807]] [5 7])
+      normalize)
 
   (insert [[1 2] [4 6] [9 11]] [2 9])
   (insert [[1 2] [4 6] [9 11]] [2 5])
@@ -162,13 +233,15 @@
   (exp->set '(< x 5))
   (exp->set '(< x -9223372036854775808))
   (exp->set '(> x 9223372036854775807))
-  (exp->set '(<= x 9223372036854775808))
+  (exp->set '(<= x 9223372036854775807))
   (exp->set '(not (< x -9223372036854775808)))
+  (exp->set '(not (> x 9223372036854775807)))
   (exp->set '(not (<= x 9223372036854775807)))
   (exp->set '(not (= x 5)))
 
   (exp->set '(and (= x 5) (= x 5)))
   (exp->set '(and (= x 5) (not (= x 5))))
+  (exp->set '(not (and (= x 5) (not (= x 5)))))
   (exp->set '(and (< x -9223372036854775808) (= y 2)))
   (exp->set '(and (<= x 9223372036854775807) (= y 2)))
   (exp->set '(and (= x 5) (and (= y 9) (not (= x 5))))) ; should be empty env
@@ -196,9 +269,35 @@
               (and (= x 15) (= y 16))))
 
 
+  (exp->set '(if (< x -9223372036854775808) (= x 5) (= x 6)))
+  (exp->set '(if (<= x 9223372036854775807) (= x 5) (= x 6)))
+  (exp->set '(if (and (>= x 5) (<= x 10))
+               (<= x 7)
+               (>= x 3)))
+  (exp->set '(and true true))
+  (exp->set '(if (= x 7) (= x 5) (= x 6)))
+  (exp->set false)
+  (env-intersection
+   (exp->set '(and (>= x 5) (<= x 10))) (exp->set '(<= x 7)))
+  (env-intersection
+   (env-complement (exp->set '(and (>= x 5) (<= x 10)))) (exp->set '(>= x 3)))
+
+  (defn f [x] (if (and (>= x 5) (<= x 10))
+                (<= x 7)
+                (>= x 3)))
+  (f Long/MIN_VALUE)
+
+  (exp->set '(and (not (and (>= x 5) (<= x 10)))
+                  (> x 0)))
+
 
   (normalize-exp '(= 4 x))
   (normalize-exp '(= x 4))
   (normalize-exp '(<= 4 x))
+
+  (negate-comparison '(= x 5))
+  (negate-comparison '(not= x 5))
+  (negate-comparison '(== x 5))
+
 
   )
